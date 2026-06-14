dcf948c7f95ff169f6b7eaa9874c48233f54e896
Ele <elenb@elens-MacBook-Air.local>
2026-06-14 20:48:10 +0400

    Deleted unused files

--- src/H1.jsx
+++
@@ -1,5 +0,0 @@
-function H1() {
-    return <h1>React Homework</h1>;
-  }
-  
-  export default H1;
--- src/Input.jsx
+++
@@ -1,5 +0,0 @@
-function Input() {
-    return <input type="text" placeholder="Enter text" />;
-  }
-  
-  export default Input;
